2805f8e773c19b4bb1d2e7211de51172f98ef180
joaquin-medina <joaquinmedinadev@gmail.com>
2022-05-10 15:13:18 -0300

    Ejercicio 1

---
+++ eje1.js
@@ -0,0 +1,10 @@
+const parImpar = (num) => {
+    (num % 2 === 0) 
+        ? console.log(`¡${num} es par!`) 
+        : console.log(`¡${num} es impar!`);
+}
+
+parImpar(10);
+parImpar(5);
+parImpar(20);
+parImpar(15);
