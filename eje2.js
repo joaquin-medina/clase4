d74c2ca2cd59cc794d6c23098dd7c85bb7728e25
joaquin-medina <joaquinmedinadev@gmail.com>
2022-05-10 15:14:02 -0300

    Ejercicio 2

---
+++ eje2.js
@@ -0,0 +1,8 @@
+const showReceivedOpposite = (num) => {
+    console.log(`Usted ingresó el número ${num}, y el opuesto es ${(num * - 1)}.`)
+}
+
+showReceivedOpposite(10);
+showReceivedOpposite(-20);
+showReceivedOpposite(50);
+showReceivedOpposite(-80);
